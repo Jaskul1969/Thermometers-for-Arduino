efc4c796f2dbf543d5fa73ef9760d46302681d70
Jaskul1969 <85389861+Jaskul1969@users.noreply.github.com>
2021-06-05 11:07:23 +0200

    Add files via upload

---
+++ TERMOMETRV1.0/TERMOMETRV1.0.ino
@@ -0,0 +1,19 @@
+
+#include "DHT.h"// that was include libray for thermometer
+#define DHT11_PIN 12// this command was define pin of thermometr for 12
+DHT dht;
+void setup() {
+ Serial.begin(9600);
+dht.setup(DHT11_PIN);
+
+}
+
+void loop() {
+ int wilgotnosc = dht.getHumidity();//in my language wilgotność is humidity
+  Serial.print(wilgotnosc);//That was showing our humidity on serial port
+  Serial.print("%RH|");  
+  int temperatura = dht.getTemperature();//in polish temperatura is tmemperature
+  Serial.print(temperatura);//Like in humidity this show temperature
+  Serial.println("*C");
+ delay(1000);
+}
